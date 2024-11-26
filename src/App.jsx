48db7aeb3ceaa9c95acf4cf54f7df118f134c719
cd ~/code/$deyvisong/dotfiles && zsh git_setup.sh <deyvisonalves@protonmail.com>
2024-11-26 11:08:58 +0100

    firstReactHello

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hi React</h1>
+      <h2>Emmet Works</h2>
+      <div className="container">
+      <p className="pabout">Meu primeiro parágrafo em React</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
